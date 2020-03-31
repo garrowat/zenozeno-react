fd4f189fe8904ad466e8562ba9207cc82c70d399
Garrett Watson <garrettbauer@gmail.com>
2020-03-31 15:13:15 +0900

    Add hooks and elements for input and quote display, test API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import { hot } from 'react-hot-loader';
 
+const ENDPOINT = process.env.REACT_APP_ENDPOINT || 'https://robotfun.us/zenozeno/quote/';
+
 const App = () => {
   const [quote, setQuote] = useState('');
-  const [input, setInput] = useState('');
+  const [inputField, setInputField] = useState('');
+
+  const getQuotes = async (endpoint) => {
+    const response = await fetch(`${endpoint}Life`)
+      .catch((error) => {
+        const message = `Error fetching quote from zenozeno API: ${error}`;
+        setQuote(message);
+      });
+
+    const quoteData = await response.json()
+      .catch((error) => {
+        const message = `Error fetching quote from zenozeno API: ${error}`;
+        setQuote(message);
+      });
+    console.log(quoteData)
+    setQuote(quoteData.quotes[2]);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getQuotes(ENDPOINT);
+  };
 
   return (
     <div>
-      <input type="text" onChange={setInput} value={input} />
-      <h2></h2>
+      <form>
+        <input
+          type="text"
+          onChange={(e) => setInputField(e.target.value)}
+          value={inputField}
+        />
+        <button type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+      <h2>{quote}</h2>
     </div>
   );
 };
