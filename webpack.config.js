7b7c72794e6a7a6382b2727f9420f93c1d7274b2
Garrett Watson <garrettbauer@gmail.com>
2020-04-01 21:57:23 +0900

    Add dotenv library and configure webpack to use it

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
 const path = require('path');
 const webpack = require('webpack');
+const dotenv = require('dotenv');
+
+const env = dotenv.config().parsed;
+
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['@babel/polyfill', './src/index.js'],
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist/'),
@@ -17,20 +25,29 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ]
           }
         }
       }
     ]
   },
-  resolve: { extensions: ['*', '.js', '.jsx'] },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+    alias: { 'react-dom': '@hot-loader/react-dom'  },
+  },
   devServer: {
     contentBase: path.join(__dirname, "public/"),
     port: 3000,
     publicPath: "http://localhost:3000/dist/",
     hotOnly: true
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin(envKeys),
+  ]
 };
 
 
