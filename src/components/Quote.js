69ac16b21a425dde592468e2ce37de74828f03c2
Garrett Watson <garrettbauer@gmail.com>
2020-04-09 17:12:15 +0900

    Refactor Quote to a component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,6 +3,8 @@ import styled, { keyframes, css } from 'styled-components';
 
 const QuoteContents = styled.span`
   display: flex;
+  justify-content: space-between;
+  align-items: center;
   padding-top: 10px;
   padding-bottom: 20px;
   padding-left: 10px;
@@ -17,34 +19,39 @@ const QuoteContents = styled.span`
 `;
 
 const FavoriteButton = styled.div`
-  flex: 0 0 30px;
-  position: relative;
-
+  flex: 0 0 15px;
+  margin-right: 10px;
+  margin-left: 5px;
+  cursor: pointer;
 `;
 
-const Heart = styled.div`
-  position: relative;
-  background-color: red;
-  height: 30px;
-  transform: rotate(-45deg);
-  width: 30px;
+const Heart = styled.svg`
+  height: 20px;
+  width: 20px;
 
-  &:after {
-    content: "";
-    background-color: red;
-    border-radius: 50%;
-    height: 30px;
-    position: absolute;
-    width: 30px;
+  & .path {
+    fill: ${({ inFavorites }) => inFavorites ? '#ff7744' : '#ebebe6'};
+
+    &:hover {
+      fill: #a8a8a3;
+    };
   };
 `;
 
-export default ({  fontSize, isLoading, children }) => {
+const SVGPath = styled.path`
+
+`;
+
+export default ({  fontSize, isLoading, children, favorites, addToFavorites }) => {
+  const inFavorites = favorites && [...children][0] in favorites;
   return (
     <QuoteContents isLoading={isLoading} fontSize={fontSize}>
       <span>{children}</span>
-      <FavoriteButton>
-        <Heart />
+      <FavoriteButton onClick={() => addToFavorites([...children][0])}>
+        <Heart viewBox="0 0 32 29.6" inFavorites={inFavorites}>
+          <path className="path" d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
+          c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z" />
+        </Heart>
       </FavoriteButton>
     </QuoteContents>
   );
