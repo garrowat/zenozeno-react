1a809499db46624a517196872027ad5decc619a7
Garrett Watson <garrettbauer@gmail.com>
2020-04-11 17:27:21 +0900

    Import useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled, {keyframes, css} from 'styled-components';
 import { createGlobalStyle } from 'styled-components'
 
